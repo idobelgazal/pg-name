6a15cfd7a94be7300d7a811c765d95420ceaf10b
idobelgazal <62871888+idobelgazal@users.noreply.github.com>
2025-06-12 13:02:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("PrimeGuard Pro Dashboard Loaded.");
+
+function showSection(id) {
+  document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
+
+function toggleMode() {
+  document.body.classList.toggle('light-mode');
+}
