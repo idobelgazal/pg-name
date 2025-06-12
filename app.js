54b5e1516606a6ce63b40a06be5f91e0410b884b
idobelgazal <62871888+idobelgazal@users.noreply.github.com>
2025-06-12 13:10:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 
 function showSection(id) {
-  document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
+  document.querySelectorAll('.section').forEach(s => s.classList.remove('active'));
   document.getElementById(id).classList.add('active');
-}
-
-function toggleMode() {
-  document.body.classList.toggle('light-mode');
 }
